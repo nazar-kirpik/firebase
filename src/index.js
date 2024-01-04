0d52eda5ac091aa2cd47f5cbcadffcb6d1affaf5
nazar-kirpik <nazarkirpik@gmail.com>
2024-01-04 21:41:01 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import {initializeApp} from 'firebase/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCQZUPHhC0WXRHaX9Y5I8nGdL2_i5AyyFM",
+    authDomain: "fir-9-dojo-6d683.firebaseapp.com",
+    projectId: "fir-9-dojo-6d683",
+    storageBucket: "fir-9-dojo-6d683.appspot.com",
+    messagingSenderId: "540945938540",
+    appId: "1:540945938540:web:c27ea8cdf0fb9c810d0049",
+    measurementId: "G-JKK8D83VJ4"
+};
+
+initializeApp(firebaseConfig)
