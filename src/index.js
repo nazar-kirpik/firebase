95418471b4aa865979e49a1e5175e643b7e58689
nazar-kirpik <nazarkirpik@gmail.com>
2024-01-05 18:56:28 +0100

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app'
 import {
-    getFirestore, collection, getDocs,
+    getFirestore, collection, onSnapshot,
     addDoc, deleteDoc, doc
 } from 'firebase/firestore'
 
@@ -24,18 +24,14 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, 'books');
 
-// get collection data
-getDocs(colRef)
-    .then((snapshot) => {
-        let books = [];
-        snapshot.docs.forEach((doc) => {
-            books.push({ ...doc.data(), id: doc.id })
-        })
-        console.log(books)
+// real time collection data
+onSnapshot(colRef, (snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+        books.push({ ...doc.data(), id: doc.id })
     })
-    .catch(err => {
-        console.log(err.message)
-    });
+    console.log(books)
+})
 
 // adding documents
 const addBookform = document.querySelector('.add')
